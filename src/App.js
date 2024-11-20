3eb2e3442b4e69c87c5259af67f56af104e7625d
Milan Pjevic <milanpjevic@icloud.com>
2024-11-20 15:12:58 +0100

    try catch finally & abort

--- src/App.js
+++ src/App.js
@@ -9,56 +9,73 @@ export default function App() {
   const [amount, setAmount] = useState("");
   const [fromCurrency, setFromCurrency] = useState("USD");
   const [toCurrency, setToCurrency] = useState("EUR");
+  const [isLoading, setIsLoading] = useState(false);
   const [convertedAmount, setConvertedAmount] = useState(null);
 
-  useEffect(
-    function () {
-      if (amount === 0 || amount === "") return;
-      if (fromCurrency === toCurrency) {
-        setConvertedAmount(amount);
-        return;
-      }
+  function handleInputValue(e) {
+    if (e.target.value === 0 || e.target.value === "") setAmount("");
+    setAmount(e.target.value);
+  }
 
-      async function convert() {
+  useEffect(() => {
+    if (amount <= 0) {
+      setConvertedAmount("");
+      return;
+    }
+    if (amount === "" || fromCurrency === toCurrency) {
+      setConvertedAmount(fromCurrency === toCurrency ? amount : 0);
+      return;
+    }
+
+    const controller = new AbortController();
+
+    async function convert() {
+      try {
+        setIsLoading(true);
+
         const res = await fetch(
-          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`,
+          { signal: controller.signal }
         );
-        const data = await res.json();
 
-        const converted = data.rates[toCurrency].toFixed(2);
+        if (!res.ok) throw new Error("Failed to fetch conversion data.");
+
+        const data = await res.json();
+        const converted = data.rates[toCurrency]?.toFixed(2) || 0;
 
         setConvertedAmount(converted);
-        console.log(converted);
+      } catch (err) {
+        if (err.name !== "AbortError") {
+          console.error(err.message);
+        }
+      } finally {
+        setIsLoading(false);
       }
-      convert();
-    },
-    [amount, fromCurrency, toCurrency]
-  );
+    }
 
-  console.log(convertedAmount);
+    convert();
+
+    return () => {
+      controller.abort();
+    };
+  }, [amount, fromCurrency, toCurrency]);
 
   return (
     <div className="app">
-      <input
-        type="number"
-        value={amount}
-        onChange={(e) => setAmount(Number(e.target.value))}
-      />
+      <input type="number" value={amount} onChange={(e) => handleInputValue(e)} />
       <select value={fromCurrency} onChange={(e) => setFromCurrency(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
-        <option value="RSD">RSD</option>
       </select>
       <select value={toCurrency} onChange={(e) => setToCurrency(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
-        <option value="RSD">RSD</option>
       </select>
-      <p>OUTPUT: {convertedAmount} </p>
+      <p>OUTPUT: {isLoading ? "Converting..." : convertedAmount} </p>
     </div>
   );
 }
